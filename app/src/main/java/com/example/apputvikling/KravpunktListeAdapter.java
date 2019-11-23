c2dea6b333e57bcb951cecf59611738a6433dca9
JJJJJJJJJJan <jan-anan@hotmail.com>
2019-11-24 01:07:54 +0200

    KravpunktListeAdapter doc

--- app/src/main/java/com/example/apputvikling/KravpunktListeAdapter.java
+++ app/src/main/java/com/example/apputvikling/KravpunktListeAdapter.java
@@ -12,12 +12,21 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import java.util.LinkedList;
 
+/**
+ * (Nærmest identisk til TilsynListeAdapter utenom onclick metoden)
+ * (Klassen er tatt/inspirert fra adapter klassen som ligger ute på canvas.)
+ * Adapter klasse for Kravpunkt recyclerview
+ * Viser informasjon for hvert Kravpunkt objekt i recycleviewet.
+ */
+
 public class KravpunktListeAdapter extends RecyclerView.Adapter<KravpunktListeAdapter.KravpunktListeHolder>{
 
+    // Deklarerer alle felt;
     private LayoutInflater inflater;
     private Context context;
     private final LinkedList<Kravpunkt> kravpunktListe;
 
+    // Konstruktør
     public KravpunktListeAdapter(Context context, LinkedList<Kravpunkt> kravpunktListe){
         this.inflater = LayoutInflater.from(context);
         this.context = context;
@@ -25,6 +34,9 @@ public class KravpunktListeAdapter extends RecyclerView.Adapter<KravpunktListeAd
 
     }
 
+    /**
+     * Metode som inflater hvert Kravpunkt objekt med "info_kort" layout
+     */
     @NonNull
     @Override
     public KravpunktListeAdapter.KravpunktListeHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
@@ -32,8 +44,17 @@ public class KravpunktListeAdapter extends RecyclerView.Adapter<KravpunktListeAd
         return new KravpunktListeAdapter.KravpunktListeHolder(itemView, this);
     }
 
+    /**
+     * Metoden henter ut felt-variablene fra dette Kravpunkt-objektet, og setter alle tekstfeltene til info_kort.xml
+     *
+     * @param holder    Referanse til indre klasse som holder på referanser til "info_kort.xml" views
+     * @param position  Posisjonen til dette Kravpunkt-elementet
+     */
     @Override
     public void onBindViewHolder(@NonNull KravpunktListeAdapter.KravpunktListeHolder holder, int position) {
+        // Vi trenger litt ekstra formatering her for å få informasjonen oversiktlig, så vi
+        // formaterer alt inn i strenger, og deretter legger det inn med setText() på de riktige objekta.
+        // Datoen ligger i objektet som 8 rene siffer, så vi må formatere inn '/' for å vise tydlig hvilken dato det er.
         String datoFersk = kravpunktListe.get(position).getDato();
         String datoFormatert = datoFersk.substring(0,2) + "/" + datoFersk.substring(2,4) + "/" + datoFersk.substring(4,8);
         String kravpunktNavn = context.getString(R.string.kravpunkt_navn) + kravpunktListe.get(position).getNavn();
@@ -45,6 +66,7 @@ public class KravpunktListeAdapter extends RecyclerView.Adapter<KravpunktListeAd
         holder.ordningsverdi.setText(ordningsverdi);
         holder.tekst.setText(tekst);
         switch (kravpunktListe.get(position).getKarakter()){
+            // Setter bilde basert på hvilken karakter kortet i position har
             case "0": case "1": holder.kort_info_karakter.setImageResource(R.drawable.smil); break;
             case "2": holder.kort_info_karakter.setImageResource(R.drawable.noytral); break;
             case "3": holder.kort_info_karakter.setImageResource(R.drawable.sur); break;
@@ -52,11 +74,19 @@ public class KravpunktListeAdapter extends RecyclerView.Adapter<KravpunktListeAd
         }
     }
 
+    /**
+     *
+     * @return størrelse på kravpunktListe
+     */
     @Override
     public int getItemCount() {
         return kravpunktListe.size();
     }
 
+    /**
+     * Klassen har ansvar for å deklarere og sette alle TextView i info_kort.xml
+     * med felt som er lagret som Kravpunkt objekt.
+     */
     class KravpunktListeHolder extends RecyclerView.ViewHolder {
 
         public final TextView kravpunktnavn;
